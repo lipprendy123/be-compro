32c863b41af4642970d16e6453fe6afb1b0c64df
Rendy <alifrendy663@gmail.com>
2025-04-10 08:52:49 +0700

    finishing configuration supabase & add middleware auth

--- internal/adapter/storage/supabase.go
+++ internal/adapter/storage/supabase.go
@@ -4,17 +4,36 @@ import (
 	"compro/config"
 	"io"
 
+	"github.com/labstack/gommon/log"
 	storage_go "github.com/supabase-community/storage-go"
 )
 
 type SupabaseInterface interface {
-	UploadFile(path string, file io.Reader) (string error)
+	// path must be filename ex: /img/photo.jpg
+	// file must be io.Reader
+	UploadFile(path string, file io.Reader) (string, error)
 }
 
 type supabaseStruct struct {
 	cfg *config.Config
 }
 
-func (s *supabaseStruct) UploadFile(path string, file io.Reader) (string error) {
-	client := storage_go.New
+// UploadFile implements SupabaseInterface.
+func (s *supabaseStruct) UploadFile(path string, file io.Reader) (string, error) {
+	client := storage_go.NewClient(s.cfg.Supabase.StorageUrl, s.cfg.Supabase.StorageKey, map[string]string{"Content-Type": "image/*"})
+
+	_, err := client.UploadFile(s.cfg.Supabase.StorageBucket, path, file)
+	if err != nil {
+		log.Errorf("Error uploading file: %v", err)
+		return "", err
+	}
+
+	result := client.GetPublicUrl(s.cfg.Supabase.StorageBucket, path)
+	return result.SignedURL, nil
+}
+
+func NewSupabase(cfg *config.Config) SupabaseInterface {
+	return &supabaseStruct{
+		cfg: cfg,
+	}
 }
